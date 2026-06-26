a2ef7ae1213e3a76d1a7be34411091c0f9e941ac
Emily Wolsek <ewolsek@gmail.com>
2026-06-25 22:30:13 -0400

    seed

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createDepartment } from "./queries/departments";
 
 await db.connect();
 await seed();
@@ -6,5 +7,11 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-    
+    for (let i= 1; i <= 5; i++) {
+        await createDepartment("Department" + i)
+    }
+    for (let i = 1; i <= 10; i++) {
+        const DepartmentId = Math.floor(Math.random() * 5) + 1;
+        await createFaculty("Faculty" + i, DepartmentId);
+    }
 }
